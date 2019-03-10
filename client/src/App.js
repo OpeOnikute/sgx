0af0baf69c0a63effa614589f6aabaa522d8d2ea
Chidi Williams <williamschidi1@gmail.com>
2019-03-10 14:47:51 +0100

    feat(client): add story invite page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.scss';
 import HomePage from './components/Pages/HomePage';
 import { Route } from 'react-router-dom';
+import InvitePage from './components/Pages/InvitePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/" component={HomePage} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/invite" exact component={InvitePage} />
       </div>
     );
   }
---
+++ client/src/components/Pages/InvitePage.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import './InvitePage.scss';
+
+class InvitePage extends Component {
+  state = {
+    userHasCopiedText: false,
+  };
+
+  onClickInviteLink = (evt) => {
+    this.copyTextToClipboard(evt.target.innerText);
+  };
+
+  fallbackCopyTextToClipboard = (text) => {
+    const textArea = document.createElement('textarea');
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+      const successful = document.execCommand('copy');
+    } catch (err) {}
+    document.body.removeChild(textArea);
+  };
+
+  copyTextToClipboard = async (text) => {
+    if (!navigator.clipboard) {
+      this.fallbackCopyTextToClipboard(text);
+      return;
+    }
+
+    try {
+      await navigator.clipboard.writeText(text);
+      this.setState({ userHasCopiedText: true });
+    } catch (error) {}
+  };
+
+  render() {
+    const { story } = this.props.location.state;
+
+    if (!story) {
+      return <Redirect to="/" />;
+    }
+
+    const { userHasCopiedText } = this.state;
+
+    return (
+      <div className="Page Page--blue">
+        <div className="Page__body">
+          <div className="Page__center-card">
+            <div className="Page__center-card__body">
+              <div className="Invite">
+                <div className="Invite__header" />
+                <div className="Invite__message">
+                  You're almost ready!
+                  <br />
+                  Share this link to invite your friend.
+                </div>
+                <div className="Invite__link__message Invite__link__message--success">
+                  {userHasCopiedText && 'Copied!'}
+                </div>
+                <button
+                  className="Invite__link"
+                  onClick={this.onClickInviteLink}
+                >
+                  {story.link}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default InvitePage;
