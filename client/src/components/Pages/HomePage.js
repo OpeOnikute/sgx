96b10319d950daacffa31ef9cd467c142b5c10d6
Chidi Williams <williamschidi1@gmail.com>
2019-03-13 00:43:39 +0100

    refactor(client): move form to inside welcome page

--- client/src/components/Pages/HomePage.js
+++ client/src/components/Pages/HomePage.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
 import './HomePage.scss';
-import CreateStoryForm from './HomePage/CreateStoryForm';
+import { createStory } from '../../api/story';
 
 class HomePage extends Component {
-  onCreateStory = ({ story }) => {
-    this.props.history.push({ pathname: '/invite', state: { story } });
+  state = {
+    name: '',
+    email: '',
+    storyTitle: '',
+    formError: '',
+  };
+
+  onSubmitForm = async (evt) => {
+    evt.preventDefault();
+    if (!this.validateForm()) return;
+    const { name, email, storyTitle } = this.state;
+    const storyResp = await createStory({ name, email, storyTitle });
+    this.props.history.push({
+      pathname: '/invite',
+      state: { story: storyResp.data },
+    });
+  };
+
+  validateForm() {
+    const { name, email, storyTitle } = this.state;
+
+    if (!name) {
+      this.setState({ formError: 'Please enter your name' });
+      return;
+    }
+    if (email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!re.test(email.toLowerCase())) {
+        this.setState({ formError: 'Please enter a valid email address' });
+        return;
+      }
+    }
+    if (!storyTitle) {
+      this.setState({ formError: 'Please enter a story title' });
+      return;
+    }
+
+    this.setState({ formError: null });
+    return true;
+  }
+
+  handleUserInput = (evt) => {
+    this.setState({ [evt.target.name]: evt.target.value });
   };
 
   render() {
+    const { formError } = this.state;
+
     return (
       <div className="Page Page--blue">
         <div className="Page__body">
@@ -21,7 +64,57 @@ class HomePage extends Component {
               </div>
             </div>
             <div className="Page__center-card__body">
-              <CreateStoryForm onCreateStory={this.onCreateStory} />
+              <form
+                className="Form Form--center-card"
+                onSubmit={this.onSubmitForm}
+              >
+                {formError && (
+                  <div className="Form__prompt Form__prompt--error">
+                    {formError}
+                  </div>
+                )}
+                <div className="Form__input-group">
+                  <label htmlFor="name" className="Form__label">
+                    What's your name?
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="Form__input"
+                    required
+                    onChange={this.handleUserInput}
+                  />
+                </div>
+                <div className="Form__input-group">
+                  <label htmlFor="email" className="Form__label">
+                    What's your email? (optional)
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="Form__input"
+                    onChange={this.handleUserInput}
+                  />
+                </div>
+                <div className="Form__input-group">
+                  <label htmlFor="storyTitle" className="Form__label">
+                    What's the title of the story?
+                  </label>
+                  <input
+                    type="text"
+                    name="storyTitle"
+                    className="Form__input"
+                    required
+                    onChange={this.handleUserInput}
+                  />
+                </div>
+
+                <div className="Form__input-group">
+                  <button type="submit" className="Form__button--submit">
+                    Start Session
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
