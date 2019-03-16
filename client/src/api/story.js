c707fdf2dcb9707bf44408d6a68e408a234c68ee
Chidi Williams <williamschidi1@gmail.com>
2019-03-16 23:40:30 +0100

    fix(client): fix request fields for create story

--- client/src/api/story.js
+++ client/src/api/story.js
@@ -1,27 +1,15 @@
-// const BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
+const BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
 
 function createStory({ name, email, storyTitle }) {
-  return Promise.resolve({
-    data: {
-      _id: '5c8836752a40710004605dac',
-      title: 'my story',
-      inviteCode: '-L_o4AnWPyQq1hz1OEiy',
-      playerOne: {
-        uid: '-L_o4AnWPyQq1hz1OEix',
-        name: 'ope',
-        email: 'ope@checl-dc.com',
-      },
-      playerTwo: { uid: '', name: '', email: '' },
-      content: null,
-      status: 'open',
-      created: '2019-03-12T22:45:09.985879148Z',
-    },
-  });
-  // return fetch(`${BASE_URL}/story`, {
-  //   method: 'POST',
-  //   body: JSON.stringify({ name, email, storyTitle }),
-  //   headers: { 'Content-Type': 'application/json' },
-  // }).then((data) => data.json());
+  return fetch(`${BASE_URL}/story`, {
+    method: 'POST',
+    body: JSON.stringify({
+      playerName: name,
+      playerEmail: email,
+      title: storyTitle,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  }).then((data) => data.json());
 }
 
 function getStory({ inviteLink }) {
